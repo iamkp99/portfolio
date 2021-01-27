4bcfcd0de545ae05809d6e31fe740a8710cea23a
Krunal Pande <krunalpande5@gmail.com>
2021-01-27 20:59:42 +0000

    First

---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Krunal Pande",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Krunal!",
+  githubUrl: "https://github.com/iamkp99",
+  linkedinUrl: "https://www.linkedin.com/in/krunal-pande-51a50a176/",
+  cvLink: "https://drive.google.com/file/d/12eIHAUhKwEfpP2zNLrly2RFRs_OO0Obx/view",
+};
